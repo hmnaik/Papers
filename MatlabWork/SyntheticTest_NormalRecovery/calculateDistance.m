1916f1699ceb65ea6ecf0fe62223abf947de26c8
hmnaik <naik@in.tum.de>
2014-04-19 17:56:56 +0200

    *fils removed

--- MatlabWork/SyntheticTest_NormalRecovery/calculateDistance.m
+++
@@ -1,7 +0,0 @@
-function dist = calculateDistance(v1 , v2 )
-
-sqDiff = (v1 - v2).^2 ; 
-
-dist = sqrt ( sum( sqDiff) ); 
-
-end
--- MatlabWork/SyntheticTest_NormalRecovery/createProjectionMatrix.m
+++
@@ -1,7 +0,0 @@
-function P = createProjectionMatrix( K , R , T)
-
-
-Rt = [R T];
-P = K * Rt ; % 3x4 
-
-end 
